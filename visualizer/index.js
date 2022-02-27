9662045bab6cd785a2ccf163be12f196153b4d6f
clairebookworm <clara32356@gmail.com>
2022-02-27 14:54:52 -0500

    viz from in

--- visualizer/index.js
+++ visualizer/index.js
@@ -82,8 +82,8 @@ gui.close();
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({ alpha: true, antialias:true });
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-// const controls = new THREE.OrbitControls(camera, renderer.domElement);
-const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
